e36a71d946280972a63dfbd7c23fc32a0752bf1c
Анатолій <visockijanatolij1@gmail.com>
2026-09-17 04:35:07 +0200

    finish burger

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+    
+});
