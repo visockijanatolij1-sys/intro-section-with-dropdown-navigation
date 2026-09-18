b820765eee622cab4d376940e602dda971f38a6f
Анатолій <visockijanatolij1@gmail.com>
2026-09-18 03:44:26 +0200

    finish overlay block

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
+const overlay = document.querySelector(".menu-overlay");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     menu.classList.toggle("active");
-    
+    overlay.classList.toggle("active");
 });
